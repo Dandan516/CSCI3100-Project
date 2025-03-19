a4d4b29c9fc44d9c31182c84a022607cc638a70a
Dennis Fung <54874611+Dandan516@users.noreply.github.com>
2025-03-19 17:03:42 +0800

    UI: portal & auth (#4)
    * Initial Setup
    
    Created the project only
    
    * Update README.md
    
    * Rendered Hello World
    
    * Updated README.md
    
    * 1
    
    * portal, auth
    
    ---------
    
    Co-authored-by: Br14n01 <brianyyhh@gmail.com>

---
+++ project/frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Portal from './components/Portal/Portal';
+import SignIn from './components/Auth/SignIn';
+import SignUp from './components/Auth/SignUp';
+
+function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/planner/`); // change later
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setData(data);
+        console.log(data)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchData();
+  }, [])
+
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Portal />} />
+          <Route path="/portal" element={<Portal />} />
+          <Route path="/auth/signin" element={<SignIn />} />
+          <Route path="/auth/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App
+
