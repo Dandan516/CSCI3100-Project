6115b81b10eaf84a63827b9e77303b2d69c97970
Br14n01 <brianyyhh@gmail.com>
2025-03-20 17:37:32 +0800

    Built Docker containers

--- project/backend/webapp/urls.py
+++ project/backend/webapp/urls.py
@@ -19,7 +19,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('planner/', include("planner.urls")),
     path('users/',  include("users.urls"))
-
 ]
