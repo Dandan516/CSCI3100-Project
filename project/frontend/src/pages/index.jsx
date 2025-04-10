e4f5c91ac866e8149052189fbca420a788b8072f
Br14n01 <brianyyhh@gmail.com>
2025-04-10 23:26:38 +0800

    Added password reset functionality

--- project/frontend/src/pages/index.jsx
+++ project/frontend/src/pages/index.jsx
@@ -3,6 +3,7 @@ export {default as SignUp} from './SignUp';
 export {default as Home} from './Home';
 export {default as Portal} from './Portal';
 export {default as ForgotPassword} from './ForgotPassword';
+export {default as PasswordReset} from './PasswordReset';
 export {default as Profile} from './Profile';
 export {default as TravelPlannerHome} from  './TravelPlannerHome';
 export {default as TravelPlanner} from './TravelPlanner';
